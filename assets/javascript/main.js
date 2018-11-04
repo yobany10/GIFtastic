d21bfc9e5abeee8dd2246600c4a057c2e5c3a6af
Yobany <yobanydesign@gmail.com>
2018-11-03 20:29:05 -0400

    added intial hero buttons with javascript

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,8 @@
 //intial heroes//
-var firstHeroes = ["captain america", "superman", "thor", "flash", "ironman"];
+var heroesArray = ["captain america", "superman", "thor", "flash", "ironman"];
 
+$(document).ready(function() {
+    for (var i = 0; i < heroesArray.length; i++) {
+        $(".gifButtons").append("<button type='button' onclick='searchGif(\"" + heroesArray[i] + "\")' class='btn btn-info' value=' " + heroesArray[i] + "'> " + heroesArray[i] + " </button>");
+    }
+});
